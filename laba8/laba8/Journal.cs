f263eb64e2750ea111bc7d51fafe09dcbcdcd9dd
maximShamigovKrutoiChel <hui1337228.nagibator>
2022-12-04 20:19:45 +0300

    na smotri

---
+++ laba8/laba8/Journal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba8
+{
+    public class Journal
+    {
+        public event EventHandler OnAdd;
+
+        public event EventHandler OnDelete;
+
+        public event EventHandler OnChange;
+
+        public event EventHandler OnSell;
+
+        public List<string> History = new List<string>();
+    }
+}
